68f07a3d6040c556f721829cff21924fa458ccb5
Benoit Seignovert <github@seignovert.fr>
2018-09-06 22:35:59 +0200

    Add fractal tholins cli output

--- aerosols/cli.py
+++ aerosols/cli.py
@@ -1,16 +1,43 @@
 # -*- coding: utf-8 -*-
 import os
 import argparse
+import numpy as np
 
+from .mie import NANG
 from .tholins import fractals_tholins
 
-def fractal_tholins(argv=None):
-    parser = argparse.ArgumentParser(description='')
+def cli_fractal_tholins(argv=None):
+    parser = argparse.ArgumentParser(description="""Fractals cross-sections and phase function for tholin aggregate.
+Use default tholins indexes (CVD) and Tomasko et al. 2008.""")
 
-    parser.add_argument('-a', '--all', action='store_true',
-                        help='Get all the output at once (disable: --limit/--page)')
-    parser.add_argument('-l', '--limit', default=10, type=int,
-                        help='Output limits (default: 10)')
-    parser.add_argument('-p', '--page', default=1, type=int,
-                        help='Page offset (default: 1)')
-    parser.add_argument('--field', help='Field key and value (sep. ` ` or `=`)', metavar='VALUE')
+    parser.add_argument('wvln', type=float, help='Wavelength (m)')
+    parser.add_argument('rm', type=float, help='Monomer radius (m)')
+    parser.add_argument('N', type=int, help='Number of monomers')
+    parser.add_argument('-df', '--fractal-dimension', type=float, default=2.0,
+                        help='Fractal dimension')
+    parser.add_argument('--nang', type=int, default=NANG,
+                        help='Number of angles for the phase function (0 -> pi/2)')
+    parser.add_argument('-f', '--force', action='store_true', 
+                        help='Bypass validity checks')
+    parser.add_argument('-p', '--phase-function', action='store_true', 
+                        help='Display the phase function')
+
+    args, others = parser.parse_known_args(argv)
+
+    try:
+        qsct, qext, qabs, gg, theta, P = fractals_tholins(
+            args.wvln, args.rm, args.fractal_dimension, args.N, args.nang, args.force)
+    except ValueError as e:
+        print(e)
+        return
+    
+    if not args.phase_function:
+        print(f"# Cross sections:")
+        print(f"Scattering: {qsct:.3e} m^-2")
+        print(f"Absorption: {qabs:.3e} m^-2")
+        print(f"Extinction: {qext:.3e} m^-2")
+    else:
+        print(f"# Phase function")
+        for t,p in zip(np.degrees(theta), P):
+            print(f"{t:.1f}\t{p:.2e}")
+
---
+++ tests/test_cli.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from aerosols.cli import cli_fractal_tholins
+
+def test_cli_fractal_tholins(capsys):
+    argv = '338e-9 60e-9 266'.split()
+    cli_fractal_tholins(argv)
+    out, err = capsys.readouterr()
+
+    stdout = """# Cross sections:
+Scattering: 2.906e-12 m^-2
+Absorption: 1.281e-12 m^-2
+Extinction: 4.187e-12 m^-2
+"""
+
+    assert out == stdout
+    assert err == ''
+
+def test_cli_fractal_tholins_phase_function(capsys):
+    argv = '-p --nang 3 338e-9 60e-9 266'.split()
+    cli_fractal_tholins(argv)
+    out, err = capsys.readouterr()
+
+    stdout = """# Phase function
+0.0\t5.87e+02
+45.0\t2.49e+00
+90.0\t5.37e-01
+135.0\t3.55e-01
+180.0\t3.70e-01
+"""
+
+    assert out == stdout
+    assert err == ''
+
+
+def test_cli_fractal_tholins_value_err(capsys):
+    argv = '-df 2.3 338e-9 60e-9 266'.split()
+    cli_fractal_tholins(argv)
+    out, err = capsys.readouterr()
+
+    stdout = "Model tested only for Df = 2 (received Df=2.30)\n"
+
+    assert out == stdout
+    assert err == ''
+
+
